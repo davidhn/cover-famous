03c8cb9164c5b25ccfe89487f6c9d2535ea640d4
David H Nguyen <davidhn1989@gmail.com>
2017-04-04 21:45:35 -0700

    filter and sort feature works now

--- src/components/Almost_Famous.vue
+++ src/components/Almost_Famous.vue
@@ -6,29 +6,28 @@
       <md-layout>
         <md-input-container>
           <label for="movie">Filter By</label>
-          <md-select name="cover-type" id="filter-by" v-model="filterBy">
-            <md-option value="all">All</md-option>
-            <md-option value="collab">Collab</md-option>
-            <md-option value="mashup">Mashup</md-option>
-            <md-option value="pop">Pop</md-option>
-            <md-option value="acoustic">Acoustic</md-option>
-            <md-option value="rnb_soul">RnB</md-option>
-            <md-option value="violin">Violin</md-option>
-            <md-option value="piano">Piano</md-option>
-            <md-option value="beatbox">Beatbox</md-option>
-            <md-option value="rock">Rock</md-option>
+          <md-select name="cover-type" id="filter-by" v-model="filterBy" @change='filterCovers($event)'>
+            <md-option value="All">All</md-option>
+            <md-option value="Collab">Collab</md-option>
+            <md-option value="Mashup">Mashup</md-option>
+            <md-option value="Pop">Pop</md-option>
+            <md-option value="Acoustic">Acoustic</md-option>
+            <md-option value="Rnb">RnB</md-option>
+            <md-option value="Violin">Violin</md-option>
+            <md-option value="Piano">Piano</md-option>
+            <md-option value="Beatbox">Beatbox</md-option>
+            <md-option value="Rock">Rock</md-option>
           </md-select>
         </md-input-container>
       </md-layout>
       <md-layout>
         <md-input-container>
           <label for="country">Sort By</label>
-          <md-select name="sort-by" id="sort-by" v-model='sortBy'>
+          <md-select name="sort-by" id="sort-by" v-model='sortBy' @change='sortCovers($event)'>
             <md-option value="trending">Trending</md-option>
-            <md-option value="cover_famous_likes">Cover Famous Likes</md-option>
-            <md-option value="cover_artist_name">Cover Artist Name</md-option>
+            <md-option value="upvotes">Upvotes</md-option>
+            <md-option value="name">Name</md-option>
             <md-option value="youtube_views">YouTube Views</md-option>
-            <md-option value="youtube_likes">YouTube Likes</md-option>
           </md-select>
         </md-input-container>
       </md-layout>
@@ -36,18 +35,15 @@
   </div>
 
   <div class='card' v-for="(cover, index) in coverSongs" v-on:click='playSong(cover.youtube_video_id, $event)'>
-    <!--<div class='card_rank'>{{ index + 1 }}</div>-->
     <div class='card_img' 
             :style="{ 'background-image' : 'url(' + cover.profile_photo + ')' }"></div>
     <div class='card-info'>
         <div class='card-artist'>
-          <!--<md-icon>volume_up</md-icon> -->
           {{ cover.name }}
         </div>
         <div class='card-song-tags'> {{ cover.tags }}</div>
         <div class='card-yt-views'><md-icon class='card-view-icon'>visibility</md-icon> {{ cover.view_count_display }}</div>
     </div>
-    <!--<div class='card-votes' v-on:click='upVote(cover.cover_song_id, cover.song_id, $event)'>-->
     <div class='card-votes' v-on:click="openDialog('dialog1')" id='custom'>
       <md-button class="md-icon-button">
         <md-icon>thumb_up</md-icon>
@@ -68,8 +64,9 @@ export default {
   name: 'almost_famous',
   data () {
     return {
-      filterBy: 'all',
+      filterBy: 'All',
       sortBy: 'trending',
+      unmutatedCoverSongs: [],
       coverSongs: [],
       voteCount: 999,
     }
@@ -78,9 +75,47 @@ export default {
     axios.get('http://localhost:3000/cover_songs')
       .then(response => {
         this.coverSongs = response.data.coverSongsList
-          .sort(function() { return 0.5 - Math.random() });
+          .sort(function() { return 0.5 - Math.random() })
+        this.unmutatedCoverSongs = response.data.coverSongsList;
       })
   },
+  methods: {
+    filterCovers: function(filter) {
+      this.coverSongs = this.unmutatedCoverSongs;
+      if (filter !== 'All') {
+        this.coverSongs = this.coverSongs.filter(function(cover) {
+          return cover.tags_array.indexOf(filter) !== -1
+        })
+      } 
+      this.$nextTick(function () {
+        // something can happen once the dom updates
+      })
+    },
+    sortCovers: function(sortCriteria) {
+      console.log(this.coverSongs);
+      if (sortCriteria === 'upvotes') {
+        this.coverSongs.sort(function(a, b) {
+          if(a.vote_count > b.vote_count) return -1;
+          if(a.vote_count < b.vote_count) return 1;
+          return 0;
+        })
+      } else if (sortCriteria === 'name') {
+        this.coverSongs.sort(function(a, b) {
+          if(a.name < b.name) return -1;
+          if(a.name > b.name) return 1;
+          return 0;
+        })
+      } else if (sortCriteria === 'youtube_views') {
+        console.log(sortCriteria)
+        this.coverSongs.sort(function(a,b) {
+          if(a.view_count > b.view_count) return -1;
+          if(a.view_count < b.view_count) return 1;
+          return 0;
+        })
+      }
+      // this.$nextTick(function() {})
+    },
+  }
 }
 </script>
 
